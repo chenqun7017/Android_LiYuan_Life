eb35ab4b30c2d1adf5a30f738e819f2a2ffb5565
chenqun@demoone.cn <321ewqdsacxz>
2017-11-07 08:25:41 +0800

    first commit

---
+++ app/src/main/java/com/lifecircle/ui/guangchang/MainActivity.java
@@ -0,0 +1,17 @@
+package com.lifecircle.ui.guangchang;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lifecircle.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
